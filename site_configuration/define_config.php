7cf68e3eff72d43b61fded642a36463dfbeaee5a
Michael Delle <mdelle@dynamo-media.com>
2018-05-17 10:15:58 -0400

    Use dotenv instead of commiting credentials into repository

--- site_configuration/define_config.php
+++ site_configuration/define_config.php
@@ -4,9 +4,9 @@
  * These variables and constants may be used in multiple pages.
  * Below we start a database connection.
  * Since PHP in moving to PDO and MySQLi, we no longer use MySQL.
- * 
+ *
  * PHP version 7+
- * 
+ *
  * @category Social
  * @package  Social
  * @author   Ziarlos <bruce.wopat@gmail.com>
@@ -14,12 +14,14 @@
  * @link     https://github.com/Ziarlos
  */
 
+$dotenv = new Dotenv\Dotenv(__DIR__, '.my_creds_file');
+$dotenv->load();
+$dotenv->required(['HOSTNAME', 'USERNAME', 'PASSWORD', 'DATABASE'])->notEmpty();
 
-define('HOSTNAME', 'localhost');
-define('USERNAME', 'a user name');
-define('PASSWORD', 'a password');
-define('DATABASE', 'a database');
-
+define('HOSTNAME', getenv('HOSTNAME'));
+define('USERNAME', getenv('USERNAME'));
+define('PASSWORD', getenv('PASSWORD'));
+define('DATABASE', getenv('DATABASE'));
 
 /**
 * Define the timezone: set to America/Los_Angeles (PST) for now.
